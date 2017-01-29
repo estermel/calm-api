88f63cc3550e881803920f1e07ce51815b8d8aa6
Ester Melati <mmm123mel@gmail.com>
2017-01-29 23:40:28 +0700

    sunday night

---
+++ include/DbHandler.php
@@ -0,0 +1,88 @@
+<?php
+
+class DbHandler {
+
+    private $conn;
+
+    function __construct() {
+        require_once dirname(__FILE__) . './DbConnect.php';
+        // opening db connection
+        $db = new DbConnect();
+        $this->conn = $db->connect();
+    }
+
+    public function register($username, $password) {
+        $stmt = $this->conn->prepare("INSERT INTO user (username, password) VALUES (?,?)");
+        $stmt->bind_param("ss", $username, $password);
+        $stmt->execute();
+        $tasks = $stmt->get_result();
+        $stmt->close();
+		return $tasks;
+    }
+
+    public function order($id_order, $id_user, $id_produk, $asrama, $no_kamar, $jus, $waktu_order, $status_order) {
+        $stmt = $this->conn->prepare("INSERT INTO orderan (id_order, id_user, id_produk, asrama, no_kamar, jus, waktu_order, status_order) VALUES (?,?,?,?,?,?,CURRENT_TIMESTAMP,?)");
+        $stmt->bind_param("iiisssss", $id_order, $id_user, $id_produk, $asrama, $no_kamar, $jus, $waktu_order, $status_order);
+        $stmt->execute();
+        $tasks = $stmt->get_result();
+        $stmt->close();
+        return $tasks;
+    }
+
+    public function confirmOrder($status_order, $waktu_konfirmasi){
+        $stmt = $this->conn->prepare("UPDATE orderan SET status_order = 'diterima' , waktu_konfirmasi = CURRENT_TIMESTAMP");
+        $stmt->execute();
+        $tasks=$stmt->get_result();
+        $stmt->close();
+        return $tasks;
+    }
+
+	public function getAkun() {
+        $stmt = $this->conn->prepare("SELECT * FROM user");
+        $stmt->execute();
+        $tasks = $stmt->get_result();
+        $stmt->close();
+        return $tasks;
+    }
+
+    public function getProduk() {
+        $stmt = $this->conn->prepare("SELECT * FROM produk");
+        $stmt->execute();
+        $tasks = $stmt->get_result();
+        $stmt->close();
+        return $tasks;
+    }
+	
+    public function getOrder() {
+        $stmt = $this->conn->prepare("SELECT * FROM orderan INNER JOIN produk ON orderan.id_produk=produk.id_produk INNER JOIN user ON orderan.username=user.username");
+        $stmt->execute();
+        $tasks = $stmt->get_result();
+        $stmt->close();
+        return $tasks;
+    }
+
+    public function getUserOrder($username) {
+        $stmt = $this->conn->prepare("SELECT * FROM orderan WHERE username='$username' ORDER BY id_order ASC");
+        $stmt->execute();
+        $tasks = $stmt->get_result();
+        $stmt->close();
+        return $tasks;
+    }
+
+    public function getUserOrderDiterima($username) {
+        $stmt = $this->conn->prepare("SELECT * FROM orderan WHERE username='$username' AND status_order='diterima' ORDER BY id_order ASC");
+        $stmt->execute();
+        $tasks = $stmt->get_result();
+        $stmt->close();
+        return $tasks;
+    }
+	
+	public function getUserOrderMenunggu($username) {
+        $stmt = $this->conn->prepare("SELECT * FROM orderan WHERE username='$username' AND status_order='menunggu' ORDER BY id_order ASC");
+        $stmt->execute();
+        $tasks = $stmt->get_result();
+        $stmt->close();
+        return $tasks;
+    }
+}
+?>
---
+++ index.php
@@ -0,0 +1,480 @@
+<?php
+
+require_once 'include/DbHandler.php';
+require_once 'include/PassHash.php';
+require 'libs/Slim/Slim.php';
+
+\Slim\Slim::registerAutoloader();
+
+$app = new \Slim\Slim();
+
+function getDB() {
+    $dbhost = "localhost"; //DB server host
+    $dbuser = "root"; //DB username
+    $dbpass = ""; //DB password
+    $dbname = "calm1"; //DB name
+
+    $mysql_conn_string = "mysql:host=$dbhost;dbname=$dbname";
+    $dbConnection = new PDO($mysql_conn_string, $dbuser, $dbpass);
+    $dbConnection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    return $dbConnection;
+}
+
+//getting all account
+$app->get('/akun', function() {
+    $app = \Slim\Slim::getInstance();
+	$app->response->setStatus(200);
+    $app->response()->headers->set('Content-Type', 'application/json');
+    $response = array();
+    $db = new DbHandler();
+    $result = $db->getAkun();
+    $response["error"] = false;
+    $response["akun"] = array();
+    while ($strData = $result->fetch_assoc()) {
+        $tmp = array();
+		$tmp["id"] = utf8_encode($strData["id"]);
+        $tmp["username"] = utf8_encode($strData["username"]);
+        array_push($response["akun"], $tmp);
+    }
+    echoRespnse(200, $response);
+});
+
+
+//getting all product
+$app->get('/produk', function() {
+    $app = \Slim\Slim::getInstance();
+    $app->response->setStatus(200);
+    $app->response()->headers->set('Content-Type', 'application/json');
+    $response = array();
+    $db = new DbHandler();
+    $result = $db->getProduk();
+    $response["error"] = false;
+    $response["produk"] = array();
+    while ($strData = $result->fetch_assoc()) {
+        $tmp = array();
+        $tmp["id_produk"] = utf8_encode($strData["id_produk"]);
+        $tmp["nama_produk"] = utf8_encode($strData["nama_produk"]);
+        $tmp["harga"] = utf8_encode($strData["harga"]);
+        $tmp["gambar"] 		= utf8_encode($strData["gambar"]);
+        $tmp["deskripsi"] 	= utf8_encode($strData["deskripsi"]);       
+        array_push($response["produk"], $tmp);
+    }
+    echoRespnse(200, $response);
+});
+
+//getting all order info
+$app->get('/order', function() {
+    $app = \Slim\Slim::getInstance();
+    $app->response->setStatus(200);
+    $app->response()->headers->set('Content-Type', 'application/json');
+    $response = array();
+    $db = new DbHandler();
+    $result = $db->getOrder();
+    $response["error"] = false;
+    $response["order"] = array();
+    while ($strData = $result->fetch_assoc()) {
+        $tmp = array();
+        $tmp["id_order"]     = utf8_encode($strData["id_order"]);
+        $tmp["username"]     = utf8_encode($strData["username"]);
+        $tmp["id_produk"]    = utf8_encode($strData["id_produk"]);
+        $tmp["nama_produk"]  = utf8_encode($strData["nama_produk"]);  
+        $tmp["asrama"]       = utf8_encode($strData["asrama"]);     
+        $tmp["no_kamar"]     = utf8_encode($strData["no_kamar"]);     
+        $tmp["jus"]          = utf8_encode($strData["jus"]);     
+        $tmp["waktu_order"]  = utf8_encode($strData["waktu_order"]);
+        $tmp["status_order"] = utf8_encode($strData["status_order"]);
+        $tmp["confirm_by"]   	  = utf8_encode($strData["confirm_by"]);
+        $tmp["waktu_konfirmasi"]  = utf8_encode($strData["waktu_konfirmasi"]);
+
+        array_push($response["order"], $tmp);
+    }
+    echoRespnse(200, $response);
+});
+
+//getting all user transaction by username
+$app->post('/userOrder', function() {
+    $app = \Slim\Slim::getInstance();
+    $id_user = $app->request->post('id_user');
+    $app->response->setStatus(200);
+    $app->response()->headers->set('Content-Type', 'application/json');
+    $response = array();
+    $db = new DbHandler();
+    $result = $db->getUserOrder($id_user);
+    $response["error"] = false;
+    $response["userOrder"] = array();
+    while ($strData = $result->fetch_assoc()) {
+        $tmp = array();
+        $tmp["id_order"]     	  = utf8_encode($strData["id_order"]);
+        $tmp["username"]           = utf8_encode($strData["username"]);
+        $tmp["id_produk"]    	  = utf8_encode($strData["id_produk"]);   
+        $tmp["asrama"]       	  = utf8_encode($strData["asrama"]);     
+        $tmp["no_kamar"]     	  = utf8_encode($strData["no_kamar"]);    
+        $tmp["jus"]          	  = utf8_encode($strData["jus"]);     
+        $tmp["waktu_order"]  	  = utf8_encode($strData["waktu_order"]);
+        $tmp["status_order"] 	  = utf8_encode($strData["status_order"]);
+        $tmp["confirm_by"]   	  = utf8_encode($strData["confirm_by"]);
+        $tmp["waktu_konfirmasi"]  = utf8_encode($strData["waktu_konfirmasi"]);
+        array_push($response["userOrder"], $tmp);
+    }
+    echoRespnse(200, $response);
+});
+
+//getting all user transaction by username with status = "diterima"
+$app->post('/userOrderDiterima', function() {
+    $app = \Slim\Slim::getInstance();
+    $username = $app->request->post('username');
+    $app->response->setStatus(200);
+    $app->response()->headers->set('Content-Type', 'application/json');
+    $response = array();
+    $db = new DbHandler();
+    $result = $db->getUserOrderDiterima($username);
+    $response["error"] = false;
+    $response["userOrderDiterima"] = array();
+    while ($strData = $result->fetch_assoc()) {
+        $tmp = array();
+        $tmp["id_order"]     	  = utf8_encode($strData["id_order"]);
+        $tmp["username"]          = utf8_encode($strData["username"]);
+        $tmp["id_produk"]    	  = utf8_encode($strData["id_produk"]);   
+        $tmp["asrama"]       	  = utf8_encode($strData["asrama"]);     
+        $tmp["no_kamar"]     	  = utf8_encode($strData["no_kamar"]);    
+        $tmp["jus"]          	  = utf8_encode($strData["jus"]);     
+        $tmp["waktu_order"]  	  = utf8_encode($strData["waktu_order"]);
+        $tmp["status_order"] 	  = utf8_encode($strData["status_order"]);
+        $tmp["confirm_by"]   	  = utf8_encode($strData["confirm_by"]);
+        $tmp["waktu_konfirmasi"]  = utf8_encode($strData["waktu_konfirmasi"]);
+        array_push($response["userOrderDiterima"], $tmp);
+    }
+    echoRespnse(200, $response);
+});
+
+//getting all user transaction by username with status = "menunggu"
+$app->post('/userOrderMenunggu', function() {
+    $app = \Slim\Slim::getInstance();
+    $username = $app->request->post('username');
+    $app->response->setStatus(200);
+    $app->response()->headers->set('Content-Type', 'application/json');
+    $response = array();
+    $db = new DbHandler();
+    $result = $db->getUserOrderMenunggu($username);
+    $response["error"] = false;
+    $response["userOrderMenunggu"] = array();
+    while ($strData = $result->fetch_assoc()) {
+        $tmp = array();
+        $tmp["id_order"]     	  = utf8_encode($strData["id_order"]);
+        $tmp["username"]          = utf8_encode($strData["username"]);
+        $tmp["id_produk"]    	  = utf8_encode($strData["id_produk"]);   
+        $tmp["asrama"]       	  = utf8_encode($strData["asrama"]);     
+        $tmp["no_kamar"]     	  = utf8_encode($strData["no_kamar"]);    
+        $tmp["jus"]          	  = utf8_encode($strData["jus"]);     
+        $tmp["waktu_order"]  	  = utf8_encode($strData["waktu_order"]);
+        $tmp["status_order"] 	  = utf8_encode($strData["status_order"]);
+        $tmp["confirm_by"]   	  = utf8_encode($strData["confirm_by"]);
+        $tmp["waktu_konfirmasi"]  = utf8_encode($strData["waktu_konfirmasi"]);
+        array_push($response["userOrderMenunggu"], $tmp);
+    }
+    echoRespnse(200, $response);
+});
+
+//logging in
+
+$app->post('/log_in', function(){
+    $app = \Slim\Slim::getInstance();
+    $username         = $app->request()->post('username');
+    $password         = $app->request()->post('password');
+    $app->response->setStatus(200);
+    $app->response()->headers->set('Content-Type', 'application/json');
+    $db = getDB();    
+    try {
+        $sth = $db->prepare("SELECT * from user WHERE username=:username AND password=:password");
+        $sth->bindParam(':username', $username, PDO::PARAM_INT);
+        $sth->bindParam(':password', $password, PDO::PARAM_INT);
+        $sth->execute();
+        $row = $sth->fetch();
+        $status_login = $row['status_login'];
+        $username_db = $row['username'];
+        $password_db = $row['password'];
+
+        if($username==$username_db && $password==$password_db){
+
+		        if($status_login=='0'){
+		            $sth1 = $db->prepare("UPDATE user SET status_login = '1' WHERE username=:username AND password=:password");
+		            $sth1->bindParam(':username', $username, PDO::PARAM_INT);
+		            $sth1->bindParam(':password', $password, PDO::PARAM_INT);
+		            $sth1->execute();
+					}        
+		            $output1 = array(
+		                    'status' => "1",
+		                    'pesan' => "log_in berhasil"
+		                );
+
+					echo json_encode($output1);
+			        $db=null;
+			        return;
+		
+		        if($status_login=='1'){
+		        	$sth2 = $db->prepare("UPDATE user SET status_login = '1' WHERE username=:username AND password=:password");
+		            $sth2->bindParam(':username', $username, PDO::PARAM_INT);
+		            $sth2->bindParam(':password', $password, PDO::PARAM_INT);
+		            $sth2->execute();
+		        	}
+		        	$output =  array('status' =>  "0",
+		        					 'pesan' => "is already logged in"
+		        					 );
+		        
+					echo json_encode($output);
+			        $db=null;
+			        return;
+			    
+		}
+
+		if($username!=$username || $password!=$password_db){
+			$output = array(
+		                    'status' => "0",
+		                    'pesan' => "username & password not match"
+		                );
+	        echo json_encode($output);
+	        $db = null;
+	        return;
+		}
+	} catch (Exception $ex) {
+		echo $ex;
+		}
+});
+
+//logout
+$app->post('/logout', function(){
+    $app = \Slim\Slim::getInstance();
+    $username         = $app->request()->post('username');
+    $app->response->setStatus(200);
+    $app->response()->headers->set('Content-Type', 'application/json');
+    $db = getDB();    
+    
+    try {
+        $sth = $db->prepare("SELECT status_login from user WHERE username=:username");
+        $sth->bindParam(':username', $username, PDO::PARAM_INT);
+        $sth->execute();
+        $row = $sth->fetch();
+        $status_login = $row['status_login'];
+        
+        if($status_login=='1'){
+		        	$sth3 = $db->prepare("UPDATE user SET status_login = '0' WHERE username=:username");
+		            $sth3->bindParam(':username', $username, PDO::PARAM_INT);
+		            $sth3->execute();
+            }
+            $output = array(
+                    'status' => "0",
+                    'pesan' => "logged out"
+                );
+
+        echo json_encode($output);
+        $db = null;
+        return;
+
+    } catch (Exception $ex) {
+        echo $ex;
+    }
+});
+
+//registering new account
+$app->post('/register', function() {
+	$app = \Slim\Slim::getInstance();
+    $nama = $app->request()->post('nama');
+    $username = $app->request()->post('username');
+    $password = $app->request()->post('password');
+    $app->response->setStatus(200);
+    $app->response()->headers->set('Content-Type', 'application/json');
+    try {
+        $db = getDB();
+        $sth = $db->prepare("SELECT COUNT(*) AS count FROM user WHERE username=:username");
+        $sth->bindParam(':username', $username, PDO::PARAM_INT);
+        $sth->execute();
+        $row = $sth->fetch();
+        if ($row['count'] > 0) {
+            $output = array(
+                'status' => "0",
+                'operation' => "username is already exist"
+            );
+            echo json_encode($output);
+            $db = null;
+            return;
+        } else {
+				$sth = $db->prepare("INSERT INTO user (nama,username,password) 
+				VALUES(:nama,:username,:password)");
+				$sth->bindParam(':nama', $nama, PDO::PARAM_INT);
+				$sth->bindParam(':username', $username, PDO::PARAM_INT);
+				$sth->bindParam(':password', $password, PDO::PARAM_INT);
+				$sth->execute();
+                $output = array(
+                    'status' => "1",
+                    'operation' => "success"
+                );
+                echo json_encode($output);
+                $db = null;
+                return;
+        }
+    } catch (Exception $ex) {
+        echo $ex;
+    }
+});
+
+//requesting order
+$app->post('/order', function() {
+    $app = \Slim\Slim::getInstance();
+    $id_order      = $app->request()->post('id_order');
+    $id_user       = $app->request()->post('id_user');
+    $id_produk     = $app->request()->post('id_produk');
+    $asrama        = $app->request()->post('asrama');
+    $no_kamar      = $app->request()->post('no_kamar');
+    $jus           = $app->request()->post('jus');
+    $waktu_order   = $app->request()->post('waktu_order');
+    $status_order  = $app->request()->post('status_order');
+    $app->response->setStatus(200);
+    $app->response()->headers->set('Content-Type', 'application/json');
+    try {
+        $db = getDB();
+        $sth = $db->prepare("INSERT INTO orderan (id_order, id_user, id_produk, asrama, no_kamar, jus, waktu_order, status_order) 
+            VALUES (:id_order, :id_user, :id_produk, :asrama, :no_kamar, :jus, CURRENT_TIMESTAMP, 'menunggu')");
+            $sth->bindParam(':id_order',     $id_order, PDO::PARAM_INT);
+            $sth->bindParam(':id_user',      $id_user, PDO::PARAM_INT);
+            $sth->bindParam(':id_produk',    $id_produk, PDO::PARAM_INT);
+            $sth->bindParam(':asrama',       $asrama, PDO::PARAM_INT);
+            $sth->bindParam(':no_kamar',     $no_kamar, PDO::PARAM_INT);
+            $sth->bindParam(':jus',          $jus, PDO::PARAM_INT);
+            $sth->execute();
+            $output = array(
+                    'status' => "1",
+                    'operation' => "success"
+                );
+                echo json_encode($output);
+                $db = null;
+                return;
+    } catch (Exception $ex) {
+        echo $ex;
+    }
+});
+
+//confirming order
+$app->post('/confirmOrder', function(){
+    $app = \Slim\Slim::getInstance();
+    $id_order         = $app->request()->post('id_order');
+    $app->response->setStatus(200);
+    $app->response()->headers->set('Content-Type', 'application/json');
+    $db = getDB();    
+    try {
+        $sth = $db->prepare("SELECT status_order from orderan WHERE id_order=:id_order");
+        $sth->bindParam(':id_order', $id_order, PDO::PARAM_INT);
+        $sth->execute();
+        $row = $sth->fetch();
+        $status_order = $row['status_order'];
+        if($status_order=='menunggu'){
+            $sth1 = $db->prepare("UPDATE orderan SET status_order = 'diterima' , waktu_konfirmasi = CURRENT_TIMESTAMP WHERE id_order=:id_order");
+            $sth1->bindParam(':id_order', $id_order, PDO::PARAM_INT);
+            $sth1->execute();
+            }
+            $output = array(
+                    'status' => "1",
+                    'pesan' => "orderan diterima"
+                );
+        echo json_encode($output);
+        $db = null;
+        return;
+    } catch (Exception $ex) {
+        echo $ex;
+    }
+});
+
+//rejecting incoming order
+$app->post('/rejectOrder', function(){
+    $app = \Slim\Slim::getInstance();
+    $id_order         = $app->request()->post('id_order');
+    $app->response->setStatus(200);
+    $app->response()->headers->set('Content-Type', 'application/json');
+    $db = getDB();    
+    try {
+        $sth = $db->prepare("SELECT status_order from orderan WHERE id_order=:id_order");
+        $sth->bindParam(':id_order', $id_order, PDO::PARAM_INT);
+        $sth->execute();
+        $row = $sth->fetch();
+        $status_order = $row['status_order'];
+        if($status_order=='menunggu'){
+            $sth1 = $db->prepare("UPDATE orderan SET status_order = 'ditolak' , waktu_konfirmasi = CURRENT_TIMESTAMP WHERE id_order=:id_order");
+            $sth1->bindParam(':id_order', $id_order, PDO::PARAM_INT);
+            $sth1->execute();
+            }
+            $output = array(
+                    'status' => "0",
+                    'pesan' => "orderan ditolak"
+                );
+        echo json_encode($output);
+        $db = null;
+        return;
+    } catch (Exception $ex) {
+        echo $ex;
+    }
+});
+
+
+/**
+ * Echoing json response to client
+ * @param String $status_code Http response code
+ * @param Int $response Json response
+ * Daftar response
+ * 200	OK
+ * 201	Created
+ * 304	Not Modified
+ * 400	Bad Request
+ * 401	Unauthorized
+ * 403	Forbidden
+ * 404	Not Found
+ * 422	Unprocessable Entity
+ * 500	Internal Server Error
+ */
+function echoRespnse($status_code, $response) {
+    $app = \Slim\Slim::getInstance();
+    // Http response code
+    $app->status($status_code);
+
+    // setting response content type to json
+    $app->contentType('application/json');
+
+    //print_r($response);
+    echo json_encode($response);
+}
+
+/**
+* Verifying required params posted or not
+*/
+    function verifyRequiredParams($required_fields) {
+        $error = false;
+        $error_fields = "";
+        $request_params = array();
+        $request_params = $_REQUEST;
+        // Handling PUT request params
+        if ($_SERVER['REQUEST_METHOD'] == 'PUT') {
+            $app = \Slim\Slim::getInstance();
+            parse_str($app->request()->getBody(), $request_params);
+            }
+        foreach ($required_fields as $field) {
+            if (!isset($request_params[$field]) ||
+                strlen(trim($request_params[$field])) <= 0) {
+                $error = true;
+                $error_fields .= $field . ', ';
+                }
+            }
+        if ($error) {
+            // Required field(s) are missing or empty
+            // echo error json and stop the app
+            $response = array();
+            $app = \Slim\Slim::getInstance();
+            $response["error"] = true;
+            $response["message"] = 'Required field(s) ' .
+            substr($error_fields, 0, -2) . ' is missing or empty';
+            echoRespnse(400, $response);
+            $app->stop();
+            }
+        }
+
+$app->run();
+?>
+
+
